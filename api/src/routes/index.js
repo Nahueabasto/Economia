5ab9cdac77f438ba5ad631b1a92ba24ed5b70f99
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-19 11:30:41 -0300

    port

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const PORT = process.env.PORT || 3000
 
 const router = Router();
 const getDolar = require("./getDolar");
