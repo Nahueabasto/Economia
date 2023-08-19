042a27f519b284ada7eaed056f3967033d6abe60
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-19 18:05:52 -0300

    ru

--- api/index.js
+++ api/index.js
@@ -1,18 +1,15 @@
-
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
 const express = require("express");
+const { conn } = require('./src/db.js');
+const server = require('./src/app.js');
+
 const app = express();
+const port = process.env.PORT || 3001;
 
-const port = process.env.PORT || 3001
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
-app.listen(port);
-console.log(`Listen on ${port}`)
-
-// Syncing all the models at once.
+// Sincronizar los modelos con la base de datos
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+  console.log('Database synced');
 });
