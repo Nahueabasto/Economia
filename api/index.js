47a4630a679547bac981fcc11834c83484981c0e
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-21 21:23:59 -0300

    vercel

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+require('dotenv').config();
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001');
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at', process.env.PORT);
   });
 });
