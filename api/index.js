28c74e90a98021246ebe2f6b4d87489ca9bcaaec
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-22 20:28:55 -0300

    install cros

--- api/index.js
+++ api/index.js
@@ -1,14 +1,3 @@
-// const server = require('./src/app.js');
-// const { conn } = require('./src/db.js');
-// require('dotenv').config();
-
-// // Syncing all the models at once.
-// conn.sync({ force: true }).then(() => {
-//   server.listen(process.env.PORT, () => {
-//     console.log('%s listening at', process.env.PORT);
-//   });
-// });
-
 const server = require('./src/app.js');
 require('dotenv').config();
 
