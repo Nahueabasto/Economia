d62a3c192c7c50dd6701d1cb2e8baf86fd04e8be
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-22 16:58:47 -0300

    a

--- api/src/db.js
+++ api/src/db.js
@@ -37,7 +37,7 @@
 
 
 
-module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
-};
+// module.exports = {
+//   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+//   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+// };
