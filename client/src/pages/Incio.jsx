0c87b3a4f86d2cf48bf6bdaad28cf5454f253304
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-22 17:15:47 -0300

    deploy back

--- client/src/pages/Incio.jsx
+++ client/src/pages/Incio.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Blue from "../components/Blue/Blue";
-import Navbar from "../components/Navbar";
+//import Navbar from "../components/Navbar";
 
 export default function Inicio() {
 
 
   return (
     <div>
-      <Navbar />
     <Blue />
   </div>
   );
