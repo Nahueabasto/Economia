899bbd2ee90473ac0166d8f2254dc1b97fd1f408
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-22 17:11:45 -0300

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //import './App.css';
 import Incio from './pages/Incio';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.baseURL = 'https://economia-dq5t.vercel.app/';
 
 function App() {
   
