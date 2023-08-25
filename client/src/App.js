909f8943959e9324a7b50c71e4033a462873f6b9
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-08-25 19:24:15 -0300

    asd

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,41 @@
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// //import './App.css';
-// import Incio from './pages/Incio';
-// import axios from 'axios';
-// axios.defaults.baseURL = 'https://economia-dq5t.vercel.app/';
-
-// function App() {
-  
-//   return (
-//     <BrowserRouter>
-//     <Switch>
-//       <Route exact path="/" component={Incio} /> 
-//     </Switch>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Incio from './pages/Inicio';
+//import './App.css';
+import Incio from './pages/Incio';
 import axios from 'axios';
+axios.defaults.baseURL = 'https://economia-dq5t.vercel.app/';
 
 function App() {
-  // Define la URL base en función de la ruta actual
-  if (window.location.hostname === 'localhost') {
-    axios.defaults.baseURL = 'http://localhost:3001';
-  } else {
-    axios.defaults.baseURL = 'https://economia-dq5t.vercel.app/';
-  }
   
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Incio} /> 
-      </Switch>
+    <Switch>
+      <Route exact path="/" component={Incio} /> 
+    </Switch>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Incio from './pages/Inicio';
+// import axios from 'axios';
+
+// function App() {
+//   // Define la URL base en función de la ruta actual
+//   if (window.location.hostname === 'localhost') {
+//     axios.defaults.baseURL = 'http://localhost:3001';
+//   } else {
+//     axios.defaults.baseURL = 'https://economia-dq5t.vercel.app/';
+//   }
+  
+//   return (
+//     <BrowserRouter>
+//       <Switch>
+//         <Route exact path="/" component={Incio} /> 
+//       </Switch>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
