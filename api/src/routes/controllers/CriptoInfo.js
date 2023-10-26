f2063e6f6534b07d441c1ddd95b6968bddff4cdf
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-26 19:07:19 -0300

    xa

--- api/src/routes/controllers/CriptoInfo.js
+++ api/src/routes/controllers/CriptoInfo.js
@@ -4,24 +4,24 @@ const getExchangeCripto = async () => {
   try {
     const response = await axios.get("https://criptoya.com/api/usdc/ars/0.5");
     console.log("Full response:", response.data);
-    
 
+    if (response.data && response.data.binance && response.data.binance.ask) {
       const exchangeCripto = {
         binance: {
           ask: response.data.binance.ask,
-        //   totalAsk: binanceData.totalAsk,
-        //   bid: binanceData.bid,
-        //   totalBid: binanceData.totalBid,
-        //   time: binanceData.time,
+          // Otros datos si es necesario
         }
       };
-
       return exchangeCripto;
-    } catch (err) {
-        console.error("Error fetching exchange rates:", err);
-        throw err; // You might want to handle the error more gracefully
-      }
-    };
+    } else {
+      console.error("Estructura de datos incorrecta en la respuesta.");
+      return null; // O maneja el error de alguna otra manera
+    }
+  } catch (err) {
+    console.error("Error fetching exchange rates:", err);
+    throw err; // Puedes manejar el error de manera más específica aquí si es necesario
+  }
+};
 
 module.exports = {
   getExchangeCripto
