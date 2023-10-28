2e41fc96d85744bb61dc8b796b46b521f3e8833a
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-10-28 19:03:29 -0300

    cripto

--- api/src/routes/controllers/CriptoInfo.js
+++ api/src/routes/controllers/CriptoInfo.js
@@ -1,29 +1,3 @@
-// const axios = require("axios");
-
-// const getExchangeCripto = async () => {
-//   try {
-//     const response = await axios.get("https://criptoya.com/api/usdt/ars/0.5");
-    
-//       const exchangeCripto = {
-//         binance: {
-//           ask: response.data.binance.ask,
-      
-//         }
-//       };
-
-//       return exchangeCripto;
-//     } catch (err) {
-//         console.error("Error fetching exchange rates:", err);
-//         throw err; // You might want to handle the error more gracefully
-//       }
-//     };
-
-// module.exports = {
-//   getExchangeCripto
-// };
-
-
-
 const axios = require("axios");
 
 const getExchangeCripto = async () => {
--- api/src/routes/getCripto.js
+++ api/src/routes/getCripto.js
@@ -1,23 +1,3 @@
-// const { Router } = require("express");
-// const router = Router();
-// const { getExchangeCripto } = require("./controllers/CriptoInfo");
-
-// router.get("/cripto", async (req, res) => {
-//     try {
-//       const exchangeData = await getExchangeCripto(); 
-//         const binanceExchange = exchangeData.binance;
-//      console.log(binanceExchange)
-//         res.json({
-//             binance: binanceExchange,
-//           });
-//     } catch (error) {
-//       // Handle any errors and send an error response
-//       res.status(500).json({ error: "An error occurred" });
-//     }
-//   });
-  
-//   // Export the router
-//   module.exports = router;
 const { Router } = require("express");
 const router = Router();
 const { getExchangeCripto } = require("./controllers/CriptoInfo");
